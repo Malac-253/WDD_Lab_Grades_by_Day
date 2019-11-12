38f24e2a97d91eb4c85425a43b4bd738af121f82
Malac-253 <54860577+Malac-253@users.noreply.github.com>
2019-11-12 09:45:49 -0500

    11/12A

--- penguins.js
+++ penguins.js
@@ -1,19 +1,60 @@
 
 var mainCode = function(classRoom){
     
-    console.log(classRoom[0].quizes)
-    var setDrawdata = function(classRoom){
+    //console.log(classRoom[0].quizes)
+    
+    //Converts the Data
+    var setDrawdata = function(classRoom,day){
         var sheet = {gh:500, gw:500, ah:540, aw:540}     
-        var classaxis = [] 
+        var classaxis = []
+        var i = 1
         classRoom.forEach(function(classRoom){
+            //console.log(classRoom.quizes[day-1].grade);
             point = {}
-            point.y = ((classRoom.grade/classRoom.max)*sheet.gh)
-            point.x = ((classRoom.day/40)*sheet.gw)
+            point.y = ((classRoom.quizes[day-1].grade/classRoom.quizes[day-1].max)*sheet.gh)
+            point.x = ((i/40)*sheet.gw)
             classaxis.push(point)
+            i = i + 1
         })
         drawGraph(classaxis,sheet)   
     }
-    //Will draw the data.
+    
+    //Creates the Buttons
+    var drawButtons = function(classRoom){
+        
+        d3.select("#tables")
+            .append("table")
+            .attr("id","buttonHolder")
+            .append("tr")
+            .attr("id","buttonHolderRow")
+        d3.select("#buttonHolderRow")
+            .selectAll("td")
+            .data(classRoom[0].quizes)
+            .enter()
+            .append("td")
+            .attr("class","main_table_rows")
+            .attr("id",function(d){return ("day"+d.day)})
+            .text(function(d){return ("Day "+d.day)})
+            .on("click",
+                function(d){
+                setDrawdata(classRoom,d.day);
+                console.log("Day "+d.day);
+                }
+                )
+        d3.select("#tables")
+            .append("table")
+            .attr("id","pageHolder")
+            .append("tr")
+            .attr("id","pageHolderRow") 
+        
+        d3.select("#tables") 
+            .append("td")
+            
+        
+        
+    }
+        
+    //Will draw the data of (x,y) point
     var drawGraph = function(data, sheet){
         console.log(data)
             d3.select("#graphs *").remove() //remove old graph 
@@ -29,35 +70,31 @@ var mainCode = function(classRoom){
                 .append("rect")
                 .attr("width",sheet.aw)
                 .attr("height",sheet.ah)
-                .attr("style","stroke-width:10; stroke:rgb(0,0,0);opacity:0.1")
+                .attr("style","stroke-width:10; stroke:rgb(0,0,0);opacity:0.05")
             d3.select("#sheet")
                 .append("rect")
-                .attr("x",2)
+                .attr("x",(sheet.ah-sheet.gh-2))
                 .attr("y",2)
-                .attr("rx",20)
-                .attr("ry",20)
                 .attr("width",sheet.gw)
                 .attr("height",sheet.gh)
-                .attr("style","fill:red;opacity:0.5")
-
+                .attr("style","fill:white")
+            
+        console.log(sheet.ah-sheet.gh)
             
             //axis x
             d3.select("#sheet")
                 .append("rect")
                 .attr("width",sheet.aw)
-                .attr("height",10)
+                .attr("height",5)
                 .attr("x",2)
                 .attr("y",sheet.aw-35)
-        
-            //axisX = d3.range(0,
-            
-        
+             
             //axis y
             d3.select("#sheet")
                 .append("rect")
-                .attr("width",10)
+                .attr("width",5)
                 .attr("height",sheet.ah)
-                .attr("x",sheet.ah-35)
+                .attr("x",(sheet.ah-sheet.gh-10))
                 .attr("y",2)
         
             //adds points
@@ -65,27 +102,21 @@ var mainCode = function(classRoom){
                 d3.select("#sheet")
                     .append("circle")
                     .attr("cy",data.y)
-                    .attr("cx",data.x)
+                    .attr("cx",data.x+(sheet.ah-sheet.gh-6))
                     .attr("r","3")
                     .attr("fill","black")               
         })      
         console.log("work")
     }
     
-    setDrawdata(classRoom[0].quizes)
-    
-    
+    drawButtons(classRoom)   
 }
 
                     
 var dataPromise = d3.json("classData.json")
-    dataPromise.then(
-        function(classRoom)
-        {
+    dataPromise.then(function(classRoom){
                 console.log(classRoom)
                 mainCode(classRoom)
-        },
-        function(err)
-        {
+        },function(err){
                 console.log("fail")
         })
